b7dca347a678fdfb7a0e1ae767e737125a5c8564
Sheliiiiy <81480883+Sheliiiiy@users.noreply.github.com>
2023-06-18 01:42:13 +0300

    UI buttons and textBox

--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -1,6 +1,7 @@
 package org.example;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JTextField;
@@ -8,26 +9,28 @@ import javax.swing.JLabel;
 
 public class UserInterface {
     public void displayMenu() {
+        FileOrganizer fileOrganizer = new FileOrganizer();
+
         // Display the menu to the user and interact with FileOrganizer class
         JFrame frame = new JFrame("File Organizer");
+        frame.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("Enter Directory Path:");
 
         // Create a text box
         JTextField textField = new JTextField();
-        textField.setBounds(20, 20, 200, 30);
-        frame.getContentPane().add(textField);
+        textField.setColumns(20);
 
         // Create a JButton
-        JButton button = new JButton("Click Me");
-        button.addActionListener(new ActionListener() {
+        JButton getExtensions = new JButton("Categorized Extensions");
+        getExtensions.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Define the actions to be performed when the button is clicked
                 System.out.println("Button clicked!");
-                FileOrganizer fileOrganizer = new FileOrganizer();
-                fileOrganizer.run();
+                fileOrganizer.getCategorizedExtensions(textField.getText());
             }
         });
-        frame.getContentPane().add(button);
 
         // Create a JButton
         JButton listFiles = new JButton("List Files In Directory");
@@ -37,14 +40,19 @@ public class UserInterface {
             public void actionPerformed(ActionEvent e) {
                 // Define the actions to be performed when the button is clicked
                 System.out.println("Button clicked!");
-                FileOrganizer fileOrganizer = new FileOrganizer();
                 fileOrganizer.listFilesInDirectory(textField.getText());//Get the entered text
             }
         });
-        frame.getContentPane().add(listFiles);
+
+        //Add elements to the Frame
+        frame.add(label);
+        frame.add(textField);
+        frame.add(getExtensions);
+        frame.add(listFiles);
 
         // Set the size and close operation of the frame
-        frame.setSize(300, 200);
+        frame.setSize((int) (Toolkit.getDefaultToolkit().getScreenSize().width * 0.5),
+                (int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.5));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Make the frame visible
